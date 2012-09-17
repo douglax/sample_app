ee31e7500321ca5e96d428c4210c7f61975acb7e
Alejandro Acosta <alxacostaa@gmail.com>
2012-09-17 11:38:42 -0600

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter   :signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_filter   :correct_user, only: [:edit, :update]
+  before_filter   :correct_user, only: [:edit, :update, :destroy]
   before_filter   :admin_user, only: :destroy
 
   def new
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts
   end
 
   def create
@@ -53,11 +54,6 @@ class UsersController < ApplicationController
 
 
   private
-    def signed_in_user
-      unless signed_in?
-        store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
 
     def correct_user
       @user = User.find(params[:id])
